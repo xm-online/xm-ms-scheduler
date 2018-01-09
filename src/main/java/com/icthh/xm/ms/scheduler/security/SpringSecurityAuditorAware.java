951395e7c344404641692e593e751d2ee9be8fd8
amedvedchuk <amedvedchuk@jbs.com.ua>
2018-01-09 11:23:04 +0200

    jhipster generated microservice app - initial commit

---
+++ src/main/java/com/icthh/xm/ms/scheduler/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,18 @@
+package com.icthh.xm.ms.scheduler.security;
+
+import com.icthh.xm.ms.scheduler.config.Constants;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of AuditorAware based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public String getCurrentAuditor() {
+        return SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM_ACCOUNT);
+    }
+}
