4a1cec2ec69bdcdd3ed0f2550f1c08c6d894d58f
vkirichenko <vitaliy.v.kirichenko@gmail.com>
2018-12-14 17:44:28 +0200

    tenant create service

--- src/main/java/com/icthh/xm/ms/scheduler/config/DatabaseConfiguration.java
+++ src/main/java/com/icthh/xm/ms/scheduler/config/DatabaseConfiguration.java
@@ -23,7 +23,6 @@ import org.hibernate.MultiTenancyStrategy;
 import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
 import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
-import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
