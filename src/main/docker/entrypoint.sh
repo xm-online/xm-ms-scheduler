4f686d1abc0daf9daa5e1aa5b5a1a329d5d3ffc3
vkirichenko <vitaliy.v.kirichenko@gmail.com>
2018-12-20 18:10:01 +0200

    refactoring

--- src/main/docker/entrypoint.sh
+++ src/main/docker/entrypoint.sh
@@ -5,4 +5,16 @@ do
     echo "set env $s"
     export "$s"="$(cat /run/secrets/$s)"
 done
+
+if [ -n "${APPLICATION_DATASOURCE_EXTERNAL_DRIVER}" ]; then
+    echo "Found external database driver ${APPLICATION_DATASOURCE_EXTERNAL_DRIVER} app.war will be modified"
+    mkdir /tmp/app
+    unzip -qq app.war -d /tmp/app
+    cp ${APPLICATION_DATASOURCE_EXTERNAL_DRIVER} /tmp/app/WEB-INF/lib
+    cd /tmp/app
+    zip -r -0 -q - . > /app.war
+    rm -rf /tmp/app
+    cd /
+fi
+
 exec java ${JAVA_OPTS} -Xmx$XMX -Djava.security.egd=file:/dev/./urandom -jar /app.war
