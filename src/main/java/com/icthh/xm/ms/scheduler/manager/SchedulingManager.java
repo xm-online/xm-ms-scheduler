fa7956eee85395612cf7f8cb242c30e1494472ff
ssenko <sergeysenja1992@gmail.com>
2018-12-27 15:21:05 +0200

    Add default ttl

--- src/main/java/com/icthh/xm/ms/scheduler/manager/SchedulingManager.java
+++ src/main/java/com/icthh/xm/ms/scheduler/manager/SchedulingManager.java
@@ -277,7 +277,6 @@ public class SchedulingManager {
     private ScheduledFuture schedule(RunnableTask expirable) {
 
         ScheduledFuture future;
-
         TaskDTO task = expirable.getTask();
 
         switch (task.getScheduleType()) {
