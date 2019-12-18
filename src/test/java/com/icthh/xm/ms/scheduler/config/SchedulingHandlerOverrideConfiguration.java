4c8bd6075df33807104b3503c630af77bae3ba81
kslivko <kslivko@jbs.com.ua>
2019-12-18 11:57:44 +0200

    [LDB] {CR#10} BE: migrate xm-ms-scheduler to Java 11

--- src/test/java/com/icthh/xm/ms/scheduler/config/SchedulingHandlerOverrideConfiguration.java
+++ src/test/java/com/icthh/xm/ms/scheduler/config/SchedulingHandlerOverrideConfiguration.java
@@ -3,6 +3,7 @@ package com.icthh.xm.ms.scheduler.config;
 import com.icthh.xm.ms.scheduler.handler.ScheduledTaskHandler;
 import com.icthh.xm.ms.scheduler.handler.ScheduledTaskHandlerMock;
 import com.icthh.xm.ms.scheduler.nameresolver.ChannelNameResolver;
+import com.icthh.xm.ms.scheduler.nameresolver.TenantAwareChannelNameResolver;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,11 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class SchedulingHandlerOverrideConfiguration {
+
+    @Bean
+    public ChannelNameResolver channelNameResolver() {
+        return new TenantAwareChannelNameResolver();
+    }
 
     @Bean
     @ConditionalOnMissingBean(ScheduledTaskHandler.class)
